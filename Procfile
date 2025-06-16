e10e106d22670689a33f3104dca7b9cb427b0f7b
fraol lemma <fraollemma0506@gmail.com>
2025-06-16 22:41:35 +0300

    fg gf

--- Procfile
+++ Procfile
@@ -1,3 +1,3 @@
 release: python manage.py migrate
-web: daphne -b 0.0.0.0 -p $PORT project.asgi:application
-worker: python manage.py runworker websocket
+web: daphne --verbosity 0 --proxy-headers --bind 0.0.0.0 --port $PORT project.asgi:application
+worker: python manage.py runworker --threads 4
--- project/wsgi.py
+++
@@ -1,8 +0,0 @@
-# project/wsgi.py
-import os
-from django.core.wsgi import get_wsgi_application
-from whitenoise import WhiteNoise
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
-application = get_wsgi_application()
-application = WhiteNoise(application, root='staticfiles')
