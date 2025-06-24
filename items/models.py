cf9802625b2e4194fa9de7d282ea099d2b68e442
fraol lemma <fraollemma0506@gmail.com>
2025-06-24 22:33:35 +0300

    user cloud

--- items/models.py
+++ items/models.py
@@ -9,12 +9,13 @@ class Item(models.Model):
     created_at = models.DateTimeField(auto_now_add=True) 
     created_by = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='items', on_delete=models.CASCADE)
     main_image = CloudinaryField('image')
+    like_count = models.PositiveIntegerField(default=0)
+    share_count = models.PositiveIntegerField(default=0)
     
     def __str__(self):
         return self.name
-    class Meta:
-        db_table = 'items_item'
 
 class SubImage(models.Model):
     item = models.ForeignKey(Item, related_name='sub_images', on_delete=models.CASCADE)
     image = CloudinaryField('image')
+    
